788e96de25284f176d5d73dcd905f4d7e02b5b81
ayyan915 <bandhack12@gmail.com>
2026-09-18 12:42:20 +0500

    make instable

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ from sockets import router as socket_router
 from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
 import model  # zaroori hai taake User model register ho jaye
+from fastapi.staticfiles import StaticFiles
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(home_router)
 app.include_router(socket_router)
+app.mount(
+    "/static",
+    StaticFiles(directory="static"),
+    name="static"
+)
